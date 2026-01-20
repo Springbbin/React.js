66681419bd9cff2d365a39aa3219292dc766aafa
Subin Park <parksubin9980@gmail.com>
2026-01-20 20:47:14 +0900

    context

---
+++ section09/src/components/List.jsx
@@ -0,0 +1,63 @@
+import "./List.css";
+import Todoitem from "./Todoitem";
+import { useState, useMemo, useContext } from "react";
+import { TodoStateContext } from "../App";
+
+const List = () => {
+  const todos = useContext(TodoStateContext);
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredData = () => {
+    if (search === "") {
+      return todos;
+    }
+    return todos.filter((todo) =>
+      todo.content.toLowerCase().includes(search.toLowerCase()),
+    );
+  };
+
+  const fillteredTodos = getFilteredData();
+
+  const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+    console.log("호출함수");
+    const totalCount = todos.length;
+    const doneCount = todos.filter((todo) => todo.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount,
+    };
+  }, [todos]);
+
+  // const { totalCount, doneCount, notDoneCount } = getAnalyzedData();
+
+  return (
+    <div className="List">
+      <h4>Todo List🌱</h4>
+      <div>
+        <div>total: {totalCount}</div>
+        <div>done: {doneCount}</div>
+        <div>notDone: {notDoneCount}</div>
+      </div>
+
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요"
+      />
+      <div className="todos_wrapper">
+        {fillteredTodos.map((todo) => {
+          return <Todoitem key={todo.id} {...todo} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default List;
